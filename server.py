e9874086a5f81f9db3d5bf1cf2d5aacce0a49602
Gerhard Stoeckel <171275975+gerhard-istari@users.noreply.github.com>
2025-07-09 10:29:24 -0400

    Added more functions

--- server.py
+++ server.py
@@ -1,6 +1,9 @@
+import json
 import os
 import random
+from io import BytesIO
 from mcp.server.fastmcp import FastMCP
+from PIL import Image
 
 from shared.constants import *
 from shared.helpers import *
@@ -37,7 +40,7 @@ def get_cameo_requirements(model_id: str) -> str:
   """Retrieves requirements for Cameo model with the specified model ID.
 
      Args:
-       mod_id (str): The ID of the model for which requirements should be retrieved.
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
   """
   try:
     req_data = download_artifact_data(model_id,
@@ -54,14 +57,14 @@ def get_3dx_parameters(model_id: str) -> str:
   """Retrieves parameters for a 3DExperience/3DX/CATIA model with the specified model ID.
 
      Args:
-       mod_id (str): The ID of the model for which parameters should be retrieved.
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
   """
   try:
     req_data = download_artifact_data(model_id,
                                       PARAM_FILE_NAME)
     ret_str = req_data.decode('utf-8')
   except FileNotFoundError:
-    ret_str = 'Requirements artifact not found. Extract the requirements from the cameo model first.'
+    ret_str = 'Parameters artifact not found. Extract parameters from the 3DX model first.'
 
   return ret_str
 
@@ -69,19 +72,22 @@ def get_3dx_parameters(model_id: str) -> str:
 @mcp.tool()
 def get_3dx_components(model_id: str) -> str:
   """Retrieves information about components such as:
-     * Materials
+     * Material
      * Center of gravity
      * Bounding box dimensions
+     * Mass
+     * Area
+     * Volume
   
      Args:
-       mod_id (str): The ID of the model for which parameters should be retrieved.
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
   """
   try:
     req_data = download_artifact_data(model_id,
                                       PARTS_FILE_NAME)
     ret_str = req_data.decode('utf-8')
   except FileNotFoundError:
-    ret_str = 'Components artifact not found. Extract the component from the 3DExperience model first.'
+    ret_str = 'Components artifact not found. Extract artifacts from the 3DExperience model first.'
 
   return ret_str
 
@@ -91,7 +97,7 @@ def extract_cameo_model_artifacts(model_id: str) -> str:
   """Extracts artifacts from a Cameo model with the specified ID.
 
      Args:
-       mod_id (str): The ID of the model for which artifacts should be extracted.
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
   """
   print('Submitting job to extract Cameo model requirements ...')
   job = submit_job(model_id = model_id,
@@ -100,17 +106,35 @@ def extract_cameo_model_artifacts(model_id: str) -> str:
                    tool_ver = CAMEO_VERSION)
   print(f"Job submitted with ID: {job.id}")
 
-  wait_for_job(job)
+  job = wait_for_job(job)
   return f"Job Complete [{job.status.name}]"
 
 
 @mcp.tool()
-def extract_3dx_model_artifacts(model_id: str,
-                                full_extract: bool) -> str:
+def extract_3dx_model_artifacts(model_id: str) -> str:
   """Extracts artifacts from a 3DExperience/3DX/CATIA model with the specified ID.
 
      Args:
-       mod_id (str): The ID of the model for which artifacts should be extracted.
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
+  """
+  print('Submitting job to extract 3DX model requirements ...')
+
+  job = submit_job(model_id = model_id,
+                   function = '@istari:extract',
+                   tool_name = CAD_TOOL_NAME)
+  print(f"Job submitted with ID: {job.id}")
+
+  job = wait_for_job(job)
+  return f"Job Complete [{job.status.name}]"
+
+
+@mcp.tool()
+def extract_3dx_model_parameters(model_id: str,
+                                 full_extract: bool) -> str:
+  """Extracts parameters from a 3DExperience/3DX/CATIA model with the specified ID.
+
+     Args:
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
        full_extract (bool): If True, all model parameters will be extracted. Otherwise, only user parameters will be extracted.
   """
   print('Submitting job to extract 3DX model requirements ...')
@@ -120,15 +144,87 @@ def extract_3dx_model_artifacts(model_id: str,
     fout.write(f"{{\"full_extract\": {str(full_extract).lower()}}}")
 
   job = submit_job(model_id = model_id,
-                   function = '@istari:extract',
+                   function = '@istari:extract_parameters',
                    tool_name = CAD_TOOL_NAME,
                    params_file = input_file)
   print(f"Job submitted with ID: {job.id}")
 
-  wait_for_job(job)
+  os.remove(input_file)
+  job = wait_for_job(job)
   return f"Job Complete [{job.status.name}]"
+
+
+@mcp.tool()
+def update_3dx_model_parameters(model_id: str,
+                                params: dict[str, str]) -> None:
+  """Updates parameters in the specified 3DExperience/3DX/CATIA model with the specified ID.
+
+     Args:
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
+       params (dict[str, str]): A dictionary containing keys with the parameter names and values with the desired parameter value.  Parameter values should include units. Actual parameter names must be used.
+  """
+  print ('Submitting job to update 3DX model parameters ...')
+
+  #params = params.replace('\\', '\\\\')
+  input_json = {'parameters': params} #json.loads(params)}
+  input_file = 'input.json'
+  with open(input_file, 'w') as fout:
+    json.dump(input_json,
+              fout)
+
+  job = submit_job(model_id = model_id,
+                   function = '@istari:update_parameters',
+                   tool_name = CAD_TOOL_NAME,
+                   params_file = input_file)
+  print(f"Job submitted with ID: {job.id}")
+
+  os.remove(input_file)
+  job = wait_for_job(job)
+
+  client = get_client()
+  mod = client.get_model(model_id);
+  with open(mod.name, 'wb') as fout:
+    fout.write(mod.file.revisions[0].read_bytes())
+
+  client.update_model(model_id,
+                      mod.name)
+
+  return f"Job Complete [{job.status.name}]"
+
+
+@mcp.tool()
+def view_3dx_model(model_id: str,
+                   view: str) -> str:
+  """Displays images of various views of a 3DExperience/3DX model.
+
+     Args:
+       model_id (str): A string containing the ID of the model for which parameters will be retrieved.
+       view (str): A string specifying the desired view of the model. Options are: 
+         * front
+         * back
+         * left
+         * right
+         * top
+         * bottom
+         * iso
+  """
+
+  ret_str = 'Image has been displayed'
+  try:
+    art_name = f"{view}_view.bmp"
+    img_data = download_artifact_data(model_id,
+                                      art_name)
+    byte_data = BytesIO(img_data)
+    img = Image.open(byte_data)
+    img.show()
+  except FileNotFoundError:
+    ret_str = 'Image artifact not found. Extract artifacts from the 3DX model first.'
+    
+  return ret_str
 
 
 if __name__ == "__main__":
     print("MCP Server is running")
     mcp.run(transport='stdio')
+    #view_3dx_model('2d7a2257-bce8-430d-b318-d22bce062fab',
+    #               'iso')
