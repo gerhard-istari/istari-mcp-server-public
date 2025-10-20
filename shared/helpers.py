1f35f3f7f8bc5ee9b91ebcc82fae1c8a40e82c84
Gerhard Stoeckel <n8440g@us.lmco.com>
2025-10-20 17:30:11 -0400

    Minor update to helper functions

--- shared/helpers.py
+++ shared/helpers.py
@@ -1,4 +1,5 @@
 import asyncio
+import os
 import random
 
 from time import sleep
@@ -160,7 +161,7 @@ def download_artifact_data(model_id: str,
           for art_rev_src in art_rev.sources:
             try:
               if art_rev_src.revision_id == mod_rev_id:
-                return get_artifact_data(art_rev)
+                return art_rev.read_bytes()
             except Exception as excp:
               print(f"Exception: {excp}")
 
